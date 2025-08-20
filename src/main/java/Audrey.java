a0383b9d38a39b84af058a3ba6dcf22c3dd7f2d2
haojun-mah <mahhaojun03@gmail.com>
2025-08-20 23:19:06 +0800

    Add audrey ascii art and welcome message

---
+++ src/main/java/Audrey.java
@@ -0,0 +1,19 @@
+public class Audrey {
+    public static void main(String[] args) {
+        String logo = "\n" +
+                "  #####  ##   ## #####  ##### ####### ##   ##\n" +
+                " ##   ## ##   ## ##  ## ##  ## ##      ##  ##\n" +
+                " ##   ## ##   ## ##  ## ##  ## ##       ## ##\n" +
+                " ####### ##   ## #####  #####  #####     ### \n" +
+                " ##   ## ##   ## ##  ## ##  ## ##          ##\n" +
+                " ##   ## ##   ## ##  ## ##  ## ##          ##\n" +
+                " ##   ##  #####  ##  ## ##  ## #######     ##\n";
+        
+        System.out.println("____________________________________________________________________");
+        System.out.println("Hello! I'm Audrey");
+        System.out.println("What can I do for you?");
+        System.out.println(logo);
+        System.out.println("____________________________________________________________________");
+        System.out.println("Bye. Hope to see you again!");
+    }
+}
