76f4b1cf7bccbf36c74e632391379b08d2aa4acd
haojun-mah <mahhaojun03@gmail.com>
2025-08-20 23:43:54 +0800

    Add audrey echoing and bye termination

--- src/main/java/Audrey.java
+++ src/main/java/Audrey.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Audrey {
     public static void main(String[] args) {
         String logo = "\n" +
@@ -14,6 +15,21 @@ public class Audrey {
         System.out.println("What can I do for you?");
         System.out.println(logo);
         System.out.println("____________________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();        
+        while (true) {
+            if ("bye".equalsIgnoreCase(input)) {
+                break;
+            } else {
+                System.out.println("    ____________________________________________________________________");
+                System.out.println("    " + input);
+                System.out.println("    ____________________________________________________________________");
+                input = scanner.nextLine();
+            }
+        }
+        scanner.close();
+        System.out.println("____________________________________________________________________");
         System.out.println("Bye. Hope to see you again!");
+        System.out.println("____________________________________________________________________");
     }
 }
