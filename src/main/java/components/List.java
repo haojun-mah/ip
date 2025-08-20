d7134f2ca4d0ff59a60cc385a5cfd4add694deda
haojun-mah <mahhaojun03@gmail.com>
2025-08-21 01:17:47 +0800

    Update showList to reflect status and announce showList mode

--- src/main/java/components/List.java
+++ src/main/java/components/List.java
@@ -1,11 +1,16 @@
 package components;
 
+import java.util.Arrays;
+
 public class List {
     private final String[] storage;
+    private final boolean[] taskCompletedStatus;
     private int count;
 
     public List() {
         storage = new String[100];
+        taskCompletedStatus = new boolean[100];
+        Arrays.fill(taskCompletedStatus, false);
         count = 0;
     }
 
@@ -16,11 +21,12 @@ public class List {
     }
 
     public String showList() {
-        String output = "";
+        String output = "Here are the tasks in your list:\n";
         for (int i = 0; i < count; i++) {
-           output += String.format("%s. %s\n", i + 1, storage[i]);
+           output += String.format("%s.[%s] %s\n", i + 1, taskCompletedStatus[i] ? 'X' : ' ', storage[i]);
         }
         return output;
     }
+   
 
 }
