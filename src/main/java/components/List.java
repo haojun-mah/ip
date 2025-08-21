7c099c173ea2d9f6895267196bc7c2310aeb8b93
haojun-mah <mahhaojun03@gmail.com>
2025-08-21 14:35:07 +0800

    Change array to collections, ArrayList

--- src/main/java/components/List.java
+++ src/main/java/components/List.java
@@ -1,17 +1,19 @@
 package components;
 
+import java.util.ArrayList;
+
 public class List {
-    private final Task[] taskStorage;
+    private final ArrayList<Task> taskStorage;
     private int count;
 
     public List() {
-        taskStorage = new Task[100]; 
+        taskStorage = new ArrayList<Task>(); 
         count = 0;
     }
 
     public String addToDos(String task) {
         Task createdTask = new Todo(task);
-        taskStorage[count] = createdTask;
+        taskStorage.add(createdTask);
         count++;
         return String.format("Got it. I've added this task:\n   %s\nNow you have %s tasks in the list.", createdTask.toString(), count);
     }
@@ -19,7 +21,7 @@ public class List {
     public String addDeadline(String task) {
         try {
             Task createdTask = new Deadline(task);
-            taskStorage[count] = createdTask;
+            taskStorage.add(createdTask);
             count++;
             return String.format("Got it. I've added this task:\n   %s\nNow you have %s tasks in the list.", createdTask.toString(), count);
         } catch (MissingDeadlineException e) {
@@ -30,7 +32,7 @@ public class List {
     public String addEvent(String task) {
         try {
             Task createdTask = new Event(task);
-            taskStorage[count] = createdTask;
+            taskStorage.add(createdTask);
             count++;
             return String.format("Got it. I've added this task:\n   %s\nNow you have %s tasks in the list.", createdTask.toString(), count);
         } catch (MissingEventException e) {
@@ -42,7 +44,7 @@ public class List {
     public String showList() {
         String output = "Here are the tasks in your list:\n";
         for (int i = 0; i < count; i++) {
-           output += String.format("%s.%s\n", i + 1, taskStorage[i]);
+           output += String.format("%s.%s\n", i + 1, taskStorage.get(i));
         }
         return output;
     }
@@ -52,8 +54,8 @@ public class List {
         if (correctedTaskIndex >= count || task <= 0) {
             return "Task does not exist!";
         }
-        taskStorage[correctedTaskIndex].markTask();
-        return String.format("Nice! I've marked this task as done!:\n   %s", taskStorage[correctedTaskIndex]);
+        taskStorage.get(correctedTaskIndex).markTask();
+        return String.format("Nice! I've marked this task as done!:\n   %s", taskStorage.get(correctedTaskIndex));
     }
 
     public String unmarkTask(int task) {
@@ -61,8 +63,8 @@ public class List {
         if (correctedTaskIndex >= count || task <= 0) {
                     return "Task does not exist!";
                 }
-        taskStorage[correctedTaskIndex].unmarkTask();
-        return String.format("Ok! I've marked this task as not done yet!:\n   %s", taskStorage[correctedTaskIndex]);
+        taskStorage.get(correctedTaskIndex).unmarkTask();
+        return String.format("Ok! I've marked this task as not done yet!:\n   %s", taskStorage.get(correctedTaskIndex));
     }
 }
  
