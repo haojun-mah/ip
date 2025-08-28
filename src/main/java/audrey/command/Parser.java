ba0879599758a8ab690713c34b29e463ee701c7f
haojun-mah <mahhaojun03@gmail.com>
2025-08-29 02:48:17 +0800

    Format switch-case in parser

--- src/main/java/audrey/command/Parser.java
+++ src/main/java/audrey/command/Parser.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import audrey.task.List;
 
 /**
- *  Parser class encapsulates command logic
+ * Parser class encapsulates command logic
  */
 public class Parser {
     private Scanner scanner;
@@ -18,6 +18,7 @@ public class Parser {
 
     /**
      * Prettier print for CLI
+     * 
      * @param string text to print
      */
     private void print(String string) {
@@ -36,20 +37,25 @@ public class Parser {
     }
 
     /**
-     *  Run command loop
+     * Run command loop
      */
     public void runInput() {
         String input = scanner.nextLine();
-        
+
         while (true) {
             if ("bye".equalsIgnoreCase(input)) {
                 break;
             } else if ("list".equalsIgnoreCase(input)) {
                 print("To Do List Activated!");
-                
+
                 while (true) {
                     input = scanner.nextLine();
-                    String[] processedInput = input.split(" ", 2); // obtain first string of words before whitespace
+                    String[] processedInput = input.split(" ", 2); // obtain
+                                                                   // first
+                                                                   // string of
+                                                                   // words
+                                                                   // before
+                                                                   // whitespace
                     String detectMark = processedInput[0];
                     Command command = Command.fromString(detectMark);
 
@@ -62,55 +68,55 @@ public class Parser {
                         break;
                     } else {
                         switch (command) {
-                            case BYE:
-                                break; // This will break out of the while loop
-                            case LIST:
-                                print(toDoList.showList());
-                                break;
-                            case MARK:
-                                try {
-                                    print(toDoList.markTask(Integer.parseInt(processedInput[1])));
-                                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                                    print("Number not provided!");
-                                }
-                                break;
-                            case UNMARK:
-                                try {
-                                    print(toDoList.unmarkTask(Integer.parseInt(processedInput[1])));
-                                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                                    print("Number not provided!");
-                                }
-                                break;
-                            case TODO:
-                                if (processedInput.length > 1) {
-                                    print(toDoList.addToDos(processedInput[1]));
-                                } else {
-                                    print("Todo description cannot be empty!");
-                                }
-                                break;
-                            case DEADLINE:
-                                if (processedInput.length > 1) {
-                                    print(toDoList.addDeadline(processedInput[1]));
-                                } else {
-                                    print("Deadline description cannot be empty!");
-                                }
-                                break;
-                            case EVENT:
-                                if (processedInput.length > 1) {
-                                    print(toDoList.addEvent(processedInput[1]));
-                                } else {
-                                    print("Event description cannot be empty!");
-                                }
-                                break;
-                            case DELETE:
-                                try {
-                                    print(toDoList.delete(Integer.parseInt(processedInput[1])));
-                                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                                    print("Number not provided!");
-                                }
-                                break;
-                            default:
-                                break;
+                        case BYE:
+                            break; // This will break out of the while loop
+                        case LIST:
+                            print(toDoList.showList());
+                            break;
+                        case MARK:
+                            try {
+                                print(toDoList.markTask(Integer.parseInt(processedInput[1])));
+                            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                                print("Number not provided!");
+                            }
+                            break;
+                        case UNMARK:
+                            try {
+                                print(toDoList.unmarkTask(Integer.parseInt(processedInput[1])));
+                            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                                print("Number not provided!");
+                            }
+                            break;
+                        case TODO:
+                            if (processedInput.length > 1) {
+                                print(toDoList.addToDos(processedInput[1]));
+                            } else {
+                                print("Todo description cannot be empty!");
+                            }
+                            break;
+                        case DEADLINE:
+                            if (processedInput.length > 1) {
+                                print(toDoList.addDeadline(processedInput[1]));
+                            } else {
+                                print("Deadline description cannot be empty!");
+                            }
+                            break;
+                        case EVENT:
+                            if (processedInput.length > 1) {
+                                print(toDoList.addEvent(processedInput[1]));
+                            } else {
+                                print("Event description cannot be empty!");
+                            }
+                            break;
+                        case DELETE:
+                            try {
+                                print(toDoList.delete(Integer.parseInt(processedInput[1])));
+                            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                                print("Number not provided!");
+                            }
+                            break;
+                        default:
+                            break;
                         }
                     }
                 }
