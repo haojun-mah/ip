a6fe35f46b4b4342f9e9dd2bdc0e114c0596129e
haojun-mah <mahhaojun03@gmail.com>
2025-08-28 23:44:11 +0800

    Create DeadlineTest

---
+++ src/test/java/audrey/task/DeadlineTest.java
@@ -0,0 +1,35 @@
+package audrey.task;
+
+import java.time.format.DateTimeParseException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import audrey.exception.MissingDeadlineException;
+
+
+public class DeadlineTest {
+   @Test
+   @DisplayName("Ensure dateline string format is within expectation")
+   public void deadline_toString() {
+    Deadline deadline = assertDoesNotThrow(() -> new Deadline("activity /by 1234-12-12"));
+    String expected = "[D][ ] activity (by:1234-12-12)";
+    String actual = deadline.toString();
+    assertEquals(expected, actual);
+   } 
+
+   @Test
+   @DisplayName("Ensure that error is thrown if no date")
+   public void deadline_noDate_throwMissingDeadlineException() {
+    assertThrows(MissingDeadlineException.class, () -> new Deadline("123 /by"));
+   }
+
+   @Test
+   @DisplayName("Ensure that error is thrown if wrong dateformat is parsed")
+   public void deadline_invalidDateFormat_throwMissingDeadlineException() {
+    assertThrows(DateTimeParseException.class, () -> new Deadline("123 /by 123-123"));
+   }
+}
